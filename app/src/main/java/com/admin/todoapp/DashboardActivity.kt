b287e8b4a61a5d9dd69ffcefaf037089ee39528f
Raynagu <rayhubli@yahoo.com>
2020-02-28 16:09:45 +0530

    Initial commit

---
+++ app/src/main/java/com/admin/todoapp/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package com.admin.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
---
+++ app/src/main/java/com/admin/todoapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.admin.todoapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        imageViewFlash.startAnimation(AnimationUtils.loadAnimation(this, R.anim.flash_in))
+        Handler().postDelayed({
+            imageViewFlash.startAnimation(AnimationUtils.loadAnimation(this, R.anim.flash_out))
+            Handler().postDelayed({
+                imageViewFlash.visibility = View.GONE
+                startActivity(Intent(this, DashboardActivity::class.java))
+            }, 500)
+        }, 1500)
+    }
+}
